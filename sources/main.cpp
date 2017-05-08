9e9373fd4b35aef8e39c96974f51b53284d84565
Paul Julien <paul.julien@epitech.eu>
2017-05-08 14:38:23 +0200

    CMAKE

---
+++ sources/main.cpp
@@ -0,0 +1,17 @@
+//
+// main.cpp for indie in /home/zgore/Projects/cpp_2017/cpp_indie_studio/sources
+// 
+// Made by Pierre Zawadil
+// Login   <pierre.zawadil@epitech.eu>
+// 
+// Started on  Mon May  8 11:13:42 2017 Pierre Zawadil
+// Last update Mon May  8 11:26:20 2017 Pierre Zawadil
+//
+
+#include <iostream>
+
+int		main(void)
+{
+  std::cout << "Paul est le plus beau <3" << std::endl;
+  return (0);
+}
