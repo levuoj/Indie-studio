0c7608e7f8aac32b69a0824a1191899673046211
Thomas Vigier <thomas.vigier@epitech.eu>
2017-05-09 18:28:02 +0200

    update player ManageGame

--- includes/Player.hpp
+++ includes/Player.hpp
@@ -5,15 +5,18 @@
 // Login   <thomas.vigier@epitech.eu>
 // 
 // Started on  Tue May  9 11:32:29 2017 thomas vigier
-// Last update Tue May  9 11:49:51 2017 thomas vigier
+// Last update Tue May  9 17:39:27 2017 thomas vigier
 //
 
 #pragma once
 
 #include "Car.hpp"
 
-class Player
+class			Player
 {
-  Car	_car;
+  Car			_car;
+  std::array<EKey>	_keys;
 public:
+
+  void			setKeys(std::array<EKey> const &);
 };
