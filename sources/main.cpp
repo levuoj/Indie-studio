5e4bba110c2a7922ed7354b9992ee71dd9a9e38d
Lebrun Kilian <kilian.lebrun@epitech.eu>
2017-05-10 15:08:59 +0200

    main for win

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,17 +1,19 @@
 //
-// main.cpp for indie in /home/zgore/Projects/cpp_2017/cpp_indie_studio/sources
+// main.cpp for main.cpp in /home/tvigier/test
 // 
-// Made by Pierre Zawadil
-// Login   <pierre.zawadil@epitech.eu>
+// Made by thomas vigier
+// Login   <thomas.vigier@epitech.eu>
 // 
-// Started on  Mon May  8 11:13:42 2017 Pierre Zawadil
-// Last update Mon May  8 11:26:20 2017 Pierre Zawadil
+// Started on  Wed May 10 11:05:32 2017 thomas vigier
+// Last update Wed May 10 11:34:42 2017 thomas vigier
 //
 
-#include <iostream>
+#include "ManageGame.hpp"
+//#include "ManageFile.hpp"
 
-int		main(void)
+int	main()
 {
-  std::cout << "Paul est le plus beau <3" << std::endl;
-  return (0);
+    ManageGame  game;
+
+    game.loadMap();
 }
