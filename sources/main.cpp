ccbec5b1a9de00d8b1d2bdc1a599734498738918
Pierre Zawadil <pierre.zawadil@epitech.eu>
2017-05-10 16:58:18 +0200

    Cmake compile with irrlicht

--- sources/main.cpp
+++ sources/main.cpp
@@ -5,13 +5,36 @@
 // Login   <pierre.zawadil@epitech.eu>
 // 
 // Started on  Mon May  8 11:13:42 2017 Pierre Zawadil
-// Last update Mon May  8 11:26:20 2017 Pierre Zawadil
+// Last update Wed May 10 15:36:43 2017 Pierre Zawadil
 //
 
-#include <iostream>
+#include <irrlicht.h>
 
 int		main(void)
 {
-  std::cout << "Paul est le plus beau <3" << std::endl;
-  return (0);
+  irr::IrrlichtDevice *device = irr::createDevice( // creation device
+						  irr::video::EDT_OPENGL,                      // l'API est OpenGL
+						  irr::core::dimension2d<irr::u32>(800,600),   // taille de la fenetre 800x600
+						  32, false, true, false, 0);
+
+  irr::video::IVideoDriver* driver =
+    device->getVideoDriver();                    // creation driver
+  irr::scene::ISceneManager *sceneManager =
+    device->getSceneManager ();                  // creation scene manager
+
+  sceneManager->addCameraSceneNode (0,             // ajout camera fixe
+				    irr::core::vector3df (0,0,0),
+				    irr::core::vector3df (5,0,0));
+
+  while (device->run()) {                          // la boucle de rendu
+    driver->beginScene(                          // demarre le rendu
+		       true,                                    // clear back-buffer
+		       true,                                    // clear z-buffer
+		       irr::video::SColor(255,255,255,255));    // fond blanc
+    sceneManager->drawAll ();                    // calcule le rendu
+    driver->endScene ();                         // affiche le rendu
+  }
+
+  device->drop ();                                 // liberation de la memoire
+  return 0;
 }
