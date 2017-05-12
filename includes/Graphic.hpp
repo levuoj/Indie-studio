65f819c62d60e7870dc28fc7d684d349820ef99a
Anthony Jouvel <anthony.jouvel@epitech.eu>
2017-05-12 14:59:47 +0200

    fonctions de base irrlicht

---
+++ includes/Graphic.hpp
@@ -0,0 +1,30 @@
+//
+// Graphic.hpp for irrlicht in /home/anthony/rendu/cpp/indie/Indie_studio/includes
+//
+// Made by Anthony Jouvel
+// Login   <anthony.jouvel@epitech.eu>
+//
+// Started on  Fri May 12 14:02:36 2017 Anthony Jouvel
+// Last update Fri May 12 14:56:59 2017 Anthony Jouvel
+//
+
+#pragma once
+
+#include <irrlicht.h>
+#include <iostream>
+
+class				Graphic
+{
+  irr::u32			_x;
+  irr::u32			_y;
+  irr::IrrlichtDevice		*_device;
+  irr::video::IVideoDriver	*_driver;
+  irr::scene::ISceneManager	*_sceneManager;
+
+  void				displayLoop();
+public:
+  Graphic(irr::u32, irr::u32);
+  Graphic();
+  ~Graphic();
+
+};
