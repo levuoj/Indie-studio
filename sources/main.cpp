360bd1d50d58e149e2718c6f5ae239d569500d5c
Pierre Zawadil <pierre.zawadil@epitech.eu>
2017-06-17 21:03:43 +0200

    env -i

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,10 +1,10 @@
 //
 // main.cpp for indie_studio in /home/zgore/Projects/cpp_2017/Indie_studio
-// 
+//
 // Made by Pierre Zawadil
 // Login   <pierre.zawadil@epitech.eu>
 // Started on  Mon May  8 11:13:42 2017 Pierre Zawadil
-// Last update Sat Jun  3 15:01:19 2017 Pashervz
+// Last update Sat Jun 17 20:53:42 2017 Pierre Zawadil
 //
 
 #include <iostream>
@@ -12,9 +12,15 @@
 
 int		main(int ac, char **av)
 {
+  if (getenv("DISPLAY") == NULL)
+    {
+      std::cerr << "Error : Some environement variable are missing." << std::endl;
+      return (EXIT_ERROR);
+    }
   if (ac != 1)
     {
       std::cerr << "Error : " << av[0] << " does not take any argument !" << std::endl;
+      return (EXIT_ERROR);
     }
   try
     {
